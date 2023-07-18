e40e1fdb10d336955f152d3298fcdfc6088722ba
Fang Yi Liu <fangyiliu@gmail.com>
2023-07-18 16:07:19 -0700

    scripts: requirement script syntax update

--- scripts/export_requirements.sh
+++ scripts/export_requirements.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 set -eux
 
-docker-compose -f docker-compose.yml run mkdocs sh -c "\
-poetry export -f requirements.txt --without-hashes > requirements.txt\
+docker-compose -f docker-compose.yml run mkdocs sh -c "set -eux;
+poetry export -f requirements.txt --without-hashes > requirements.txt;
 "
-set +x
 #docker-compose -f docker-compose.yml run mkdocs sh -c "poetry update &&
 #poetry export -f requirements.txt --without-hashes > requirements.txt --with dev"
