a2eb2c7fda617615053573bd3e6f55c37a2a3e69
Fang Yi Liu <fangyiliu@gmail.com>
2025-11-19 23:09:01 -0800

    chore: migrate from poetry to uv
    To run locally
    uv sync
    uv run mkdocs serve

--- scripts/export_requirements.sh
+++ scripts/export_requirements.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 set -eux
 
-docker-compose exec mkdocs sh -c "set -eux;
-poetry export -f requirements.txt > requirements.txt;
-"
-#docker-compose -f docker-compose.yml run mkdocs sh -c "poetry update &&
-#poetry export -f requirements.txt > requirements.txt --with dev"
+# Assumes
+#   - uv is installed
+#   - this script is run from the root of the repo
+uvx uv pip compile pyproject.toml -o requirements.txt
